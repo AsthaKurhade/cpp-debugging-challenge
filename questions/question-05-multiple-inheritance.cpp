90af41489a0a6741894086ade6d646d422ab33c5
SrushtiArgade122 <srushtiargade5@gmail.com>
2026-08-12 16:09:49 +0530

    Add TeachingAssistant class with multiple inheritance
    Implemented a TeachingAssistant class that inherits from both Student and Employee classes. The class includes methods to display personal and role information.

--- questions/question-05-multiple-inheritance.cpp
+++ questions/question-05-multiple-inheritance.cpp
@@ -1,1 +1,111 @@
+//A college teaching assistant is both: a Student ,an Employee
+//Both roles share common information from Person.
 
+#include <iostream>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+
+public:
+
+    Person(string n)
+    {
+        name = n;
+        cout << "Person created" << endl;
+    }
+
+    void showPerson()
+    {
+        cout << "Name: " << name << endl;
+    }
+};
+
+class Student : public Person
+{
+protected:
+    int rollNo;
+
+public:
+
+    Student(string n, int r)
+        : Person(n)
+    {
+        rollNo = r;
+        cout << "Student created" << endl;
+    }
+
+    void showRole()
+    {
+        cout << "Student Roll No: "
+             << rollNo << endl;
+    }
+};
+
+class Employee : public Person
+{
+protected:
+    int employeeId;
+
+public:
+
+    Employee(string n, int id)
+        : Person(n)
+    {
+        employeeId = id;
+        cout << "Employee created" << endl;
+    }
+
+    void showRole()
+    {
+        cout << "Employee ID: "
+             << employeeId << endl;
+    }
+};
+
+class TeachingAssistant :
+    public Student,
+    public Employee
+{
+private:
+    int hours;
+
+public:
+
+    TeachingAssistant(
+        string n,
+        int r,
+        int id,
+        int h)
+        : Student(n, r),
+          Employee(n, id)
+    {
+        hours = h;
+        cout << "Teaching Assistant created"
+             << endl;
+    }
+
+    void display()
+    {
+        showPerson();
+        showRole();
+
+        cout << "Working hours: "
+             << hours << endl;
+    }
+};
+
+int main()
+{
+    TeachingAssistant ta(
+        "Aarav",
+        124,
+        501,
+        12);
+
+    ta.display();
+
+    return 0;
+}
