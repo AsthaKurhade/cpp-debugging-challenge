a7055a8011a9a6e620a40c1f206cd58a02f0259d
SrushtiArgade122 <srushtiargade5@gmail.com>
2026-08-12 16:07:50 +0530

    Update question-03-gcd-lcm.cpp

--- questions/question-03-gcd-lcm.cpp
+++ questions/question-03-gcd-lcm.cpp
@@ -1,1 +1,74 @@
+// Two machines in a laboratory require maintenance at fixed intervals.The program calculates:
+//GCD of maintenance intervals
+//LCM to determine when both machines require maintenance together
+//prime factors of the common maintenance period
 
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return b;
+
+    return gcd(a % b, b);
+}
+
+int lcm(int a, int b)
+{
+    return a * b / gcd(a, b);
+}
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void printPrimeFactors(int n)
+{
+    cout << "Prime factors: ";
+
+    for (int i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            cout << n << " ";
+            n = n / i;
+        }
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int machineA, machineB;
+
+    cout << "Enter maintenance intervals: ";
+    cin >> machineA >> machineB;
+
+    int commonDivisor = gcd(machineA, machineB);
+    int commonInterval = lcm(machineA, machineB);
+
+    cout << "GCD: " << commonDivisor << endl;
+    cout << "Next common maintenance after: "
+         << commonInterval << " days" << endl;
+
+    if (isPrime(commonInterval))
+        cout << "Common interval is prime" << endl;
+    else
+        cout << "Common interval is not prime" << endl;
+
+    printPrimeFactors(commonInterval);
+
+    return 0;
+}
